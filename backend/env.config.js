e4931b5f518d965d771e8af2dc8e20d7e018eb74
kk36 <lk0981@mail.ru>
2023-08-03 17:34:54 +0300

    env server port fix

--- backend/env.config.js
+++ backend/env.config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const {
-  SERVER_PORT = 3001,
+  SERVER_PORT = 3000,
   DB = 'mongodb://127.0.0.1:27017/mestodb',
   SECRET_KEY = 'some-secret-key',
   NODE_ENV = 'development',
